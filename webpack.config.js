f41a8c96e2a0b32fbd7594523307b9e353d674e1
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-04-14 18:58:54 +0400

    Change webpack config file for current project needs

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    //clean: true,
-  },
-  devServer: {
-    static: './dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|css)$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		static: './dist',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sass|css)$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
 };
