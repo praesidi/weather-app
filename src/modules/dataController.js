e755ca80a80075b7ba884afc0c388da47e161b52
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-04-17 20:44:59 +0400

    Add functions to get and set weather format

--- src/modules/dataController.js
+++ src/modules/dataController.js
@@ -1,52 +1,75 @@
 const dataController = (() => {
 	const apiKey = '52a94c4a83484cb1a15122652230904';
-	const defaultUserLocation = 'Tokyo';
 	let userLocation;
+	let currentFormat = 'c';
+
+	const setWeatherFormat = (value) => {
+		currentFormat = value;
+	};
+
+	const getWeatherFormat = () => {
+		return currentFormat;
+	};
+
+	const setUserLocation = (position) => {
+		userLocation = position;
+		//console.log(userLocation);
+	};
+
+	const getUserLocation = () => {
+		return userLocation;
+	};
 
 	const getWeatherData = async () => {
 		try {
 			const response = await fetch(
-				`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${defaultUserLocation}}`
+				`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${userLocation}&aqi=no&alerts=no`
 			);
 			const data = await response.json();
-			console.log(data);
+			const { location, current } = data;
+			const dataObj = {
+				city: location.name,
+				region: location.region,
+				country: location.country,
+				condition: current.condition.text,
+				icon: current.condition.icon,
+				feelslike_c: current.feelslike_c,
+				feelslike_f: current.feelslike_f,
+				humidity: current.humidity,
+				last_updated: current.last_updated,
+				precip_in: current.precip_in,
+				precip_mm: current.precip_mm,
+				temp_c: current.temp_c,
+				temp_f: current.temp_f,
+				wind_kph: current.wind_kph,
+				wind_mph: current.wind_mph,
+			};
+
+			return dataObj;
 		} catch (err) {
 			console.log(err);
 		}
 	};
 
-	const getConditionList = async () => {
-		try {
-			const response = await fetch(
-				'https://www.weatherapi.com/docs/weather_conditions.json'
-			);
-			const list = await response.json();
-			console.log(list);
-		} catch (err) {
-			console.log(err);
-		}
+	const handleUserCoordinates = (position) => {
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		const userCoordinates = `${latitude},${longitude}`;
+
+		setUserLocation(userCoordinates);
 	};
 
-	// const saveUserLocation = (position) => {
-	// 	userLocation = position;
-	// };
-
-	// const handleLocationRequestError = (error) => {
-	// 	//userLocation = defaultUserLocation;
-	// 	console.log(error);
-	// };
-
-	// const requestUserLocation = () => {
-	// 	navigator.geolocation.getCurrentPosition(
-	// 		saveUserLocation,
-	// 		handleLocationRequestError
-	// 	);
-	// };
+	const requestUserLocation = () => {
+		navigator.geolocation.getCurrentPosition(handleUserCoordinates);
+	};
 
 	return {
-		getWeatherData,
-		getConditionList,
-		//requestUserLocation,
+		handleUserCoordinates,
+		requestUserLocation,
+		getUserLocation,
+		setUserLocation,
+		getWeatherFormat,
+		setWeatherFormat,
 		getWeatherData,
 	};
 })();
