75291ae5b9c605a0069b89ef7cf6bcfbe18e02be
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-04-17 20:42:07 +0400

    Show weather if a user allows to track his location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles/main.sass';
+import dataController from './modules/dataController';
+import domController from './modules/domController';
+//import handlersController from './modules/handlersController';
+
+async function handleGeolocation(position) {
+	dataController.handleUserCoordinates(position);
+	const weatherData = await dataController.getWeatherData();
+	console.log(weatherData);
+	domController.populateWeatherContainer(
+		weatherData,
+		dataController.getWeatherFormat()
+	);
+}
+
+function getCurrentPosition() {
+	navigator.geolocation.getCurrentPosition(handleGeolocation);
+}
+
+getCurrentPosition();
